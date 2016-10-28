096febc4214c2fefedac47c98146710e9afb2a4d
shaoxia <wu8854123@163.com>
2016-10-28 13:22:31 +0800

    0.1

---
+++ src/main/java/com/shaoxia/mapper/MediaMapper.java
@@ -0,0 +1,9 @@
+package com.shaoxia.mapper;
+
+import com.shaoxia.pojo.Media;
+
+import tk.mybatis.mapper.common.Mapper;
+
+public interface MediaMapper extends Mapper<Media>{
+	
+}
---
+++ src/main/java/com/shaoxia/pojo/Media.java
@@ -0,0 +1,120 @@
+package com.shaoxia.pojo;
+
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 多媒体资源表
+ * @author wu885
+ *
+ */
+@Table(name="shaoxia_media")
+public class Media {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String resourceUrl;
+	
+	private String resourcePath;
+	
+	private String resourceName;
+	
+	private String resourceSuffix;
+	
+	private String storageLocation;
+	
+	private String cloudServer;
+	
+	private String resourceType;
+	
+	private Date created;
+	
+	private Date updated;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getResourceUrl() {
+		return resourceUrl;
+	}
+
+	public void setResourceUrl(String resourceUrl) {
+		this.resourceUrl = resourceUrl;
+	}
+
+	public String getResourcePath() {
+		return resourcePath;
+	}
+
+	public void setResourcePath(String resourcePath) {
+		this.resourcePath = resourcePath;
+	}
+
+	public String getResourceName() {
+		return resourceName;
+	}
+
+	public void setResourceName(String resourceName) {
+		this.resourceName = resourceName;
+	}
+
+	public String getResourceSuffix() {
+		return resourceSuffix;
+	}
+
+	public void setResourceSuffix(String resourceSuffix) {
+		this.resourceSuffix = resourceSuffix;
+	}
+
+	public String getStorageLocation() {
+		return storageLocation;
+	}
+
+	public void setStorageLocation(String storageLocation) {
+		this.storageLocation = storageLocation;
+	}
+
+	public String getCloudServer() {
+		return cloudServer;
+	}
+
+	public void setCloudServer(String cloudServer) {
+		this.cloudServer = cloudServer;
+	}
+
+	public String getResourceType() {
+		return resourceType;
+	}
+
+	public void setResourceType(String resourceType) {
+		this.resourceType = resourceType;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
+	
+}
